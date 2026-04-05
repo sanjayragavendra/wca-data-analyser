b247027b04d8e9b14d7af0fab1025c6e2c1e91a5
sanjayragavendra <95925810+sanjayragavendra@users.noreply.github.com>
2026-04-05 20:07:50 +0530

    Add files via upload

---
+++ init.py
@@ -0,0 +1,7 @@
+"""WCA Analysis Package"""
+
+__version__ = "1.0.0"
+__author__ = "WCA Analysis Team"
+
+from wca_analysis.data.data_loader import WCADataLoader
+from wca_analysis.analysis.competitor_analyzer import CompetitorAnalyzer
