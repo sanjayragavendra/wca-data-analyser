c763904f4cbaf11d5ed3543738cd301010ad7d3b
sanjayragavendra <95925810+sanjayragavendra@users.noreply.github.com>
2026-04-05 19:59:35 +0530

    Create init.py

---
+++ wca_analysis/analysis/init.py
@@ -0,0 +1,8 @@
+"""Analysis modules"""
+
+from wca_analysis.analysis.competitor_analyzer import CompetitorAnalyzer
+from wca_analysis.analysis.global_analyzer import GlobalAnalyzer
+from wca_analysis.analysis.country_analyzer import CountryAnalyzer
+from wca_analysis.analysis.event_analyzer import EventAnalyzer
+
+__all__ = ['CompetitorAnalyzer', 'GlobalAnalyzer', 'CountryAnalyzer', 'EventAnalyzer']
